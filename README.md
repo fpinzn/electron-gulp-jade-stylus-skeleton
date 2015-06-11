e4a251163eaa9b5bf2973d11ca142387a451836b
Francisco Pinzon <hello@pacho.me>
2015-06-11 11:56:23 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#electron-gulp-jade-stylus-skeleton
+
+Skeleton app for electron based applications using jade and stylus.
+
+Source files located in `src` directory.
+
+`gulp build:dist` will build stylus and jade files into `dist` dir.
---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+// imports
+var gulp = require('gulp');
+var electron = require('gulp-electron');
+var stylus = require('gulp-stylus');
+var del = require('del');
+var jade = require('gulp-jade');
+
+var packageJson = require('./package.json');
+
+
+
+gulp.task('clean:dist', function(cb) {
+    del('./dist', cb);
+})
+
+gulp.task('clean:release', function(cb) {
+    del('./release', cb);
+})
+
+// Get one .styl file and render
+gulp.task('stylus', function () {
+  gulp.src('./src/styles/main.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./dist/styles'));
+});
+
+gulp.task('jade', function() {
+  gulp.src('./src/views/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('./dist/views'))
+});
+
+gulp.task('copy:assets', function(){
+    gulp.src(['./src/assets/*', './src/assets/**/*'])
+    .pipe(gulp.dest('./dist/assets'))
+})
+
+gulp.task('copy:js', function(){
+    gulp.src('**/*.js', {base: './src'})
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('copy:package.json', function(){
+    gulp.src('./package.json')
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('electron', ['build:dist'] ,function() {
+    gulp.src("")
+    .pipe(electron({
+        src: './dist',
+        packageJson: packageJson,
+        release: './release',
+        cache: './cache',
+        version: 'v0.26.0',
+        packaging: true,
+        platforms: ['darwin-x64']
+    }))
+    .pipe(gulp.dest(""));
+});
+
+gulp.task('build:dist', ['stylus', 'jade', 'copy:js', 'copy:package.json', 'copy:assets'])
